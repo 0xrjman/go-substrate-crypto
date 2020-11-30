ffd89c0609ea3eb84c5fec7e374ef3c1bcc5cc4d
flynn <fujun.jiang@rylink.com>
2020-11-30 19:37:00 +0800

    fix: ed25519 curve

--- crypto/substrate_key.go
+++ crypto/substrate_key.go
@@ -8,9 +8,9 @@ import (
 )
 
 const (
-	EcdsaType = iota
+	Ed25519Type = iota
 	Sr25519Type
-	Ed25519
+	EcdsaType
 )
 
 /*
@@ -23,11 +23,11 @@ date: 2020-09-01
 func GenerateSubstrateKey(curveType int) ([]byte, []byte, error) {
 	switch curveType {
 	case 0:
-		return nil, nil, fmt.Errorf("unsupport ecdsa,type=%d", curveType)
+		return ed25519.GenerateKey()
 	case 1:
 		return sr25519.GenerateKey()
 	case 2:
-		return ed25519.GenerateKey()
+		return nil, nil, fmt.Errorf("unsupport ecdsa curve type %d to create key", curveType)
 	default:
 		return nil, nil, fmt.Errorf("unsupport curve type %d to create key", curveType)
 	}
@@ -43,11 +43,11 @@ date: 2020-09-01
 func GenerateSubstrateKeyBySeed(priv []byte, curveType int) ([]byte, error) {
 	switch curveType {
 	case 0:
-		return nil, fmt.Errorf("unsupport ecdsa curve type %d to create key", curveType)
+		return ed25519.GenerateKeyBySeed(priv)
 	case 1:
 		return sr25519.GenerateKeyBySeed(priv)
 	case 2:
-		return ed25519.GenerateKeyBySeed(priv)
+		return nil, fmt.Errorf("unsupport ecdsa curve type %d to create key", curveType)
 	default:
 		return nil, fmt.Errorf("unsupport curve type %d to create key", curveType)
 	}
@@ -70,11 +70,12 @@ date: 2020-09-01
 func Sign(privateKey, message []byte, curveType int) ([]byte, error) {
 	switch curveType {
 	case 0:
-		return nil, fmt.Errorf("unsupport ecdsa curve type %d to sign", curveType)
+		return ed25519.Sign(privateKey, message)
+
 	case 1:
 		return sr25519.Sign(privateKey, message)
 	case 2:
-		return ed25519.Sign(privateKey, message)
+		return nil, fmt.Errorf("unsupport ecdsa curve type %d to sign", curveType)
 	default:
 		return nil, fmt.Errorf("unsupport curve type %d to sign", curveType)
 	}
